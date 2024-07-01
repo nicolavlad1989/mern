6354969b6691702ec0f3e6c7f652dbf9eb1f1bd7
Nicola Vlad <vladnicola4@gmail.com>
2024-07-01 17:58:32 +0300

    connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';   // dotenv Security: Keeps sensitive information out of the source code, helps prevent accidental exposure of sensitive information.
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Conected to MongoDB')
+}).catch((err) => {
+    console.log(err);
+}) ;                                 //connect to MongoDB,   
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
+
