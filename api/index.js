8c294ce3bd075948cb6e216e0382c2d0be1079df
Nicola Vlad <vladnicola4@gmail.com>
2024-07-02 15:31:10 +0300

    Add a middelware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -22,3 +22,13 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next ) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server Error';
+    return res.status(statusCode).json({
+        succes: false,
+        message,
+        statusCode,
+    });
+}); //middelware
